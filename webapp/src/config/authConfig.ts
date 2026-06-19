79009864bd1de0353af62d9e8876866f2ea6ff10
Yasiru Edirimanna <yasirue@yasirue.local>
2026-06-19 13:03:45 +0530

    Add react webapp scaffold for GRC platform

---
+++ webapp/src/config/authConfig.ts
@@ -0,0 +1,38 @@
+// Copyright (c) 2026 WSO2 LLC. (https://www.wso2.com).
+//
+// WSO2 LLC. licenses this file to you under the Apache License,
+// Version 2.0 (the "License"); you may not use this file except
+// in compliance with the License.
+// You may obtain a copy of the License at
+//
+// http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing,
+// software distributed under the License is distributed on an
+// "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+// KIND, either express or implied.  See the License for the
+// specific language governing permissions and limitations
+// under the License.
+
+import "@config/portalConfig";
+
+export interface AuthConfig {
+  baseUrl: string;
+  clientId: string;
+  signInRedirectURL: string;
+  signOutRedirectURL: string;
+}
+
+const getAuthConfig = (): AuthConfig => {
+  const config = window.config;
+  return {
+    baseUrl: config?.GRC_PLATFORM_AUTH_BASE_URL || "",
+    clientId: config?.GRC_PLATFORM_AUTH_CLIENT_ID || "",
+    signInRedirectURL:
+      config?.GRC_PLATFORM_AUTH_SIGN_IN_REDIRECT_URL || window.location.origin + "/",
+    signOutRedirectURL:
+      config?.GRC_PLATFORM_AUTH_SIGN_OUT_REDIRECT_URL || window.location.origin + "/",
+  };
+};
+
+export const authConfig = getAuthConfig();
